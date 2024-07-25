8b4e3c6a208d42c5a280e74c1ab2c278302d8e09
NDanczuk <nicolasdanc@gmail.com>
2024-07-25 09:05:13 -0300

    Section 5 done

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,15 @@
+import { EntityRepository, Repository } from "typeorm/index.js";
+import Product from "../entities/Product";
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
+  }
+}
