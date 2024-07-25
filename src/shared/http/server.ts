517d75b414b608557071d780b4e2ed072d152007
NDanczuk <nicolasdanc@gmail.com>
2024-07-25 10:27:41 -0300

    Fix DB Connection

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,11 @@
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import cors from "cors";
+import { errors } from "celebrate";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
+import "@shared/typeorm";
 
 const app = express();
 
@@ -9,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
@@ -18,7 +24,7 @@ app.use(
         message: error.message,
       });
     }
-
+    console.log(error);
     return response.status(500).json({
       status: "error",
       message: "Internal server error",
