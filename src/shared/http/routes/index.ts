58222e397ed6462ecaf2fd0915c20948da562b03
NDanczuk <nicolasdanc@gmail.com>
2024-07-25 13:45:11 -0300

    Created users router

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import productsRouter from "@modules/products/routes/products.routes";
+import usersRouter from "@modules/users/users.routes";
 
 const routes = Router();
 
 routes.use("/products", productsRouter);
+
+routes.use("/users", usersRouter);
 
 routes.get("/", (request, response) => {
   return response.json({ message: "Hello!" });
