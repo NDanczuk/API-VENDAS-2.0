62ec85f90aac40136f24c7a4056f1e6f8f84f147
NDanczuk <nicolasdanc@gmail.com>
2024-07-26 15:06:50 -0300

    Show Profile Service added

--- src/modules/users/services/ShowProfileService.ts
+++ src/modules/users/services/ShowProfileService.ts
@@ -1,14 +1,22 @@
 import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import User from "../typeorm/entities/User";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+}
 
 class ListUserService {
-  public async execute(): Promise<User[]> {
+  public async execute({ user_id }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const users = await usersRepository.find();
+    const user = await usersRepository.findById(user_id);
 
-    return users;
+    if (!user) {
+      throw new AppError("User not found!");
+    }
+    return user;
   }
 }
 
