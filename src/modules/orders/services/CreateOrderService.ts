46c4f7942148da629ee43d4c3696ae2efe64299f
NDanczuk <nicolasdanc@gmail.com>
2024-07-29 14:54:25 -0300

    Orders router added

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -73,7 +73,7 @@ class CreateOrderService {
     const updatedProductQuantity = order_products.map(product => ({
       id: product.product_id,
       quantity:
-        existProducts.filter(p => p.id === product.id)[0].quantity -
+        existProducts.filter(p => p.id === product.product_id)[0].quantity -
         product.quantity,
     }));
 
