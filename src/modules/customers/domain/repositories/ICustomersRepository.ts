d89d38a31099732b30563ff3f9bd1d620b715430
NDanczuk <nicolasdanc@gmail.com>
2024-07-30 11:54:44 -0300

    Added customers repository interface

---
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -0,0 +1,14 @@
+import { ICustomer } from "../models/ICostumer";
+// import { ICreateCustomer } from "../models/ICreateCustomer";
+
+export interface ICustomersRepository {
+  findByName(name: string): Promise<ICustomer | undefined>;
+
+  findById(id: string): Promise<ICustomer | undefined>;
+
+  findByEmail(email: string): Promise<ICustomer | undefined>;
+
+  // create(data: ICreateCustomer): Promise<ICustomer>;
+
+  // save(customer: ICustomer): Promise<ICustomer>;
+}
