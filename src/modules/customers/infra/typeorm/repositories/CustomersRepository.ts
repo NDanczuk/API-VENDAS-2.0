7a9c89875c7627d7fc3486b56e4e9a830e4c66fc
NDanczuk <nicolasdanc@gmail.com>
2024-07-30 16:41:34 -0300

    Updated CustomersRepository

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -2,6 +2,7 @@ import { getRepository, Repository } from "typeorm";
 import Customer from "../entities/Customer";
 import { ICustomersRepository } from "@modules/customers/domain/repositories/ICustomersRepository";
 import { ICreateCustomer } from "@modules/customers/domain/models/ICreateCustomer";
+import { ICustomer } from "@modules/customers/domain/models/ICostumer";
 
 class CustomersRepository implements ICustomersRepository {
   private ormRepository: Repository<Customer>;
@@ -22,6 +23,16 @@ class CustomersRepository implements ICustomersRepository {
     await this.ormRepository.save(customer);
 
     return customer;
+  }
+
+  public async remove(customer: Customer): Promise<void> {
+    await this.ormRepository.remove(customer);
+  }
+
+  public async findAll(): Promise<ICustomer[] | undefined> {
+    const customers = await this.ormRepository.find();
+
+    return customers;
   }
 
   public async findByName(name: string): Promise<Customer | undefined> {
