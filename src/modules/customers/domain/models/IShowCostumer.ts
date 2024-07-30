2295b2f56f093eec793c480eb14ffb37b9130127
NDanczuk <nicolasdanc@gmail.com>
2024-07-30 16:34:00 -0300

    Added customer models interfaces

--- src/modules/customers/domain/models/IShowCostumer.ts
+++
@@ -1,3 +0,0 @@
-export interface IShowCostumer {
-  id: string;
-}
--- src/modules/customers/domain/models/IUpdateCustomer.ts
+++ src/modules/customers/domain/models/IUpdateCustomer.ts
@@ -1,4 +1,4 @@
-export interface IUpdateCostumer {
+export interface IUpdateCustomer {
   id: string;
   name: string;
   email: string;
