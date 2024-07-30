44ab4ef1b60e0d547e39a9fdc77d9935f6c19c4e
NDanczuk <nicolasdanc@gmail.com>
2024-07-30 14:12:43 -0300

    dependencie inversion

--- src/modules/customers/infra/http/controllers/CustomersController.ts
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -4,6 +4,7 @@ import UpdateCustomerService from "../../../services/UpdateCustomerService";
 import CreateCustomerService from "../../../services/CreateCustomerService";
 import ShowCustomerService from "../../../services/ShowCustomerService";
 import ListCustomerService from "../../../services/ListCustomerService";
+import CustomersRepository from "../../typeorm/repositories/CustomersRepository";
 
 export default class CustomersController {
   // List all customers
@@ -28,7 +29,9 @@ export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
 
-    const createCustomer = new CreateCustomerService();
+    const customersRepository = new CustomersRepository();
+
+    const createCustomer = new CreateCustomerService(customersRepository);
 
     const customer = await createCustomer.execute({
       name,
